1b77c2fe730430e11fee2ab882181e8ff256f293
Jared Tulayan <2lyNJ.E@gmail.com>
2018-01-03 19:44:26 -0500

    Add initial project files

---
+++ lwjgl3/src/main/java/com/jtulayan/main/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,23 @@
+package com.jtulayan.main.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.jtulayan.main.Runner;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+    public static void main(String[] args) {
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new Runner(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("gdx-chess");
+        configuration.setWindowedMode(640, 480);
+        return configuration;
+    }
+}
