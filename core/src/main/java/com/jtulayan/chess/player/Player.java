c1095a6b6d13c3e7615963dfcddebec665865ced
Jared Tulayan <2lyNJ.E@gmail.com>
2019-02-24 10:33:08 -0500

    Make Player an interface again

--- core/src/main/java/com/jtulayan/chess/player/Player.java
+++ core/src/main/java/com/jtulayan/chess/player/Player.java
@@ -1,16 +1,13 @@
+package com.jtulayan.chess.player;
+
 /**
  * General interface for any object that can call out moves to make on the chessboard.
  */
-public abstract class Player {
-    public boolean isWhite = false;
+public interface Player {
 
     /**
      * Method to get the move from this player.
      * @return a move, notated as "<origin_space><dest_space>"
      */
-    public abstract String createMove();
-
-    public boolean getIsWhite() {
-        return isWhite;
-    }
+    String createMove();
 }
